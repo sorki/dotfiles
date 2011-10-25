fb7268c1757adcc804a007a81dce0bb0a1f637ca
Richard Marko <rissko@gmail.com>
2011-10-25 18:19:16 +0200

    Xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -47,32 +47,65 @@ myManageHook = composeAll
     [ manageHook gnomeConfig
     , className =? "MPlayer"           --> doFloat
     , className =? "Gimp"              --> doFloat
-    , className =? "Pidgin"            --> doShift "im"
-    , className =? "Skype0"            --> doShift "im"
-    , className =? "Sonata"            --> doShift "music"
-    , className =? "Thunderbird"       --> doShift "mail"
-    , className =? "google-chrome"  --> doShift "web"
+    , className =? "Pidgin"            --> doShift "3"
+    , className =? "Skype0"            --> doShift "3"
+    , className =? "Sonata"            --> doShift "5"
+    , className =? "Thunderbird"       --> doShift "6"
+    , className =? "google-chrome"     --> doShift "1"
     , resource  =? "desktop_window"    --> doIgnore
     , resource  =? "kdesktop"          --> doIgnore
-    , title     =? "irssi"             --> doShift "irssi"
+    , className =? "stalonetray"       --> doIgnore
+    , title     =? "irssi"             --> doShift "~"
 
     , isFullscreen                     --> (doF W.focusDown <+> doFullFloat) 
     ] <+> manageDocks
 
 -- Colors
-dzenFg = "#222222"
-dzenBg = "#edecec"
-dzenFont = "Sans:bold:size=9"
-dzenWidth = 16
-dzenCommon = "-xs 1 -h "++(show dzenWidth)++" -bg '"++dzenBg++"' -fg '"++dzenFg++"' -fn '"++dzenFont++"'"
+black = "#000000"
+white = "#ffffff"
 
+dzenFg = white
+dzenBg = black
+dzenFont = "Sans:bold:size=10"
+dzenWidth = 24
+dzenCommon = "-xs 0 -h "++(show dzenWidth)++" -bg '"++dzenBg++"' -fg '"++dzenFg++"' -fn '"++dzenFont++"'"
+
+ppFg  = "#faaaaa" -- FG
+ppBg  = black
+ppVFg = "#3a0a0d" -- Visible (xinerama)
+ppVBg = black
+ppHFg = "#555555" -- Hidden no windows
+ppUFg = "#780000" -- Urgent
+ppUBg = white
+iFg   = white     -- Icon
+iBg   = black
+ppSFg = "#171717" -- Separator
+bord  = "#000000" -- Border
+bordF = "#00dd00" -- Focused Border
+
+iDir = "/home/rmarko/.xmonad/icons"
+
+myLogHook h = dynamicLogWithPP $ defaultPP
+      {   ppCurrent  = dzenColor ppFg ppBg . dzenStrip
+        , ppVisible  = dzenColor ppVFg ppVBg . dzenStrip
+        , ppHidden          = dzenColor dzenFg dzenBg . dzenStrip
+        , ppHiddenNoWindows = dzenColor ppHFg dzenBg . dzenStrip
+        , ppUrgent          = dzenColor ppUFg ppUBg . wrap ("^i(" ++ iDir ++ "/info_03.xbm)") "" . dzenStrip
+        , ppWsSep    = ""
+        , ppSep      = " "
+        , ppLayout   = nostr
+        , ppTitle    = nostr
+        , ppOutput   = hPutStrLn h
+      } where nostr x = ""
 
 m1Width = 1280
 m1Height = 800
 
-lbarOffset = 33
-statusRatio = (5%6)
+lbarOffset = 0
+statusRatio = (1%9)
 bottomRatio = (1%2)
+
+rbarOffset = 24*8
 
 ltopBarWidth = floor $ fromRational $ statusRatio * (fromInteger (m1Width - lbarOffset))
 rtopBarWidth = ceiling $ fromRational $ (1 - statusRatio) * (fromInteger  (m1Width - lbarOffset))
@@ -81,16 +114,17 @@ lbotBarWidth = floor $ fromRational $ bottomRatio * (fromInteger (m1Width - lbar
 rbotBarWidth = ceiling $ fromRational $ (1 - bottomRatio) * (fromInteger  (m1Width - lbarOffset))
 
 ltopBar = "dzen2 -x "++ (show lbarOffset) ++" -w "++ (show ltopBarWidth) ++" -ta l -sa l " ++ dzenCommon
-rtopBar  = "conky -c ~/.xmonad/conkytop | dzen2 -x "++ (show $ ltopBarWidth + lbarOffset) ++" -w "++ (show rtopBarWidth) ++" -ta r -sa l " ++ dzenCommon
+--rtopBar  = "conky -c ~/.xmonad/conkytop | dzen2 -x "++ (show $ ltopBarWidth + lbarOffset) ++" -w "++ (show rtopBarWidth) ++" -ta r -sa l " ++ dzenCommon
+rtopBar  = "~/.xmonad/dzen_top | dzen2 -x "++ (show $ ltopBarWidth + lbarOffset) ++" -w "++ (show $ rtopBarWidth - rbarOffset) ++" -ta l -sa l " ++ dzenCommon
 lbotBar  = "conky -c ~/.xmonad/conkylbot | dzen2 -y "++(show $ m1Height - dzenWidth) ++" -x "++ (show lbarOffset) ++" -w "++ (show lbotBarWidth)++" -ta l -sa l " ++ dzenCommon
 rbotBar  = "conky -c ~/.xmonad/conkyrbot | dzen2 -y "++(show $ m1Height - dzenWidth)++" -x "++ (show $ lbotBarWidth + lbarOffset) ++" -w "++ (show rbotBarWidth) ++" -ta r -sa l " ++ dzenCommon
 
-botBar = "conky -c ~/.xmonad/conkytop | dzen2 -xs 1 -x '0' -y '785' -h '20' -w '1280' -ta 'r' " ++ dzenCommon
+botBar = "conky -c ~/.xmonad/conkytop | dzen2 -x '0' -y '780' -h '20' -w '1280' -ta 'r' " ++ dzenCommon
 
 main = do
     statusBar <- spawnPipe ltopBar
-    rtBar <- spawnPipe rtopBar
-    bBar <- spawnPipe botBar
+--    rtBar <- spawnPipe rtopBar
+--    bBar <- spawnPipe botBar
 --    lbBar <- spawnPipe lbotBar
 --    rbBar <- spawnPipe rbotBar
     xmonad $ withUrgencyHook NoUrgencyHook
@@ -99,24 +133,24 @@ main = do
         modMask            = mod1Mask,
         focusFollowsMouse  = True,
         numlockMask        = mod2Mask,
-        workspaces         = ["irssi", "web","con","im","dev","music","mail","latex","8","9", "0"],
-        normalBorderColor  = "#000000",
-        focusedBorderColor = "#00dd00",
+        workspaces         = ["~", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "="],
+        normalBorderColor  = bord,
+        focusedBorderColor = bordF,
         keys               = myKeys,
         mouseBindings      = myMouseBindings,
         layoutHook         = avoidStruts $ smartBorders $ myLayout,
         manageHook         = myManageHook,
 --      startupHook        = myStartupHook >> gnomeRegister >> startupHook desktopConfig,
-        logHook            = myLogHook statusBar
+        logHook            = myLogHook statusBar,
+        startupHook        = unsafeSpawn ". $HOME/.xinitrc"
 
     }
 
-myLayout = onWorkspace "web" (noBorders Full ||| tall ) $ onWorkspace "im" imLayout $ onWorkspace "0" Grid $ std
+myLayout = onWorkspace "1" (noBorders Full ||| tall ) $ onWorkspace "3" imLayout $ std
     where
       std = (tall ||| Mirror tall ||| noBorders Full)
-      tall = Tall 1 (3/100) (1/2) 
+      tall = Tall 1 (3/100) (1/2)
       imtall = Tall 2 (3/100) (1/2)
-
       imLayout = reflectHoriz $ withIM pidginRatio pidginRoster $ reflectHoriz $ withIM skypeRatio skypeRoster (imtall ||| Mirror imtall ||| Grid)
       pidginRatio  = (1%7)
       pidginRoster = And (ClassName "Pidgin") (Role "buddy_list")
@@ -126,7 +160,7 @@ myLayout = onWorkspace "web" (noBorders Full ||| tall ) $ onWorkspace "im" imLay
 -- X prompt
 myXPConfig = defaultXPConfig
     {
-        font  = "-*-terminus-*-*-*-*-12-*-*-*-*-*-*-u"
+        font  = "-*-terminus-*-*-*-*-18-*-*-*-*-*-*-u"
         ,fgColor = "#00dd00"
         , bgColor = "#000000"
         , borderColor = "#00dd00"
@@ -147,6 +181,7 @@ searchEngineMap method = M.fromList $
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
     , ((modm .|. shiftMask, xK_c     ), kill)
+    , ((modm .|. shiftMask, xK_End   ), io (exitWith ExitSuccess))
     , ((modm,               xK_space ), sendMessage NextLayout)
     , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
     , ((modm,               xK_n     ), refresh)
@@ -174,12 +209,11 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     ]
     ++
     [((m .|. modm, k), windows $ f i)
-        | (i, k) <- zip (XMonad.workspaces conf) ([xK_grave] ++ [xK_1 .. xK_9] ++ [xK_0])
+        | (i, k) <- zip (XMonad.workspaces conf) ([xK_grave] ++ [xK_1 .. xK_9] ++ [xK_0, xK_minus, xK_equal])
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
     ++
-
     [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_q, xK_w] [0..] -- w, q for xinerama
+        | (key, sc) <- zip [xK_w, xK_q] [0..] -- w, q for xinerama
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 
@@ -193,15 +227,3 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm, button4), (\w ->focus w >>  sendMessage Shrink))
     , ((modm, button5), (\w ->focus w >>  sendMessage Expand))
     ]
-
-myLogHook h = dynamicLogWithPP $ defaultPP
-      {   ppCurrent  = dzenColor "black" "" . pad
-        , ppVisible  = dzenColor "purple" "" . pad
-        , ppHidden   = dzenColor "#777777" "" . pad
-        , ppHiddenNoWindows = dzenColor "white"  "" . pad
-        , ppUrgent   = dzenColor "#dd0000" "" . dzenStrip
-        , ppWsSep    = ""
-        , ppSep      = " | "
-        , ppTitle    = (" " ++) . dzenColor "" "" . dzenEscape
-        , ppOutput   = hPutStrLn h
-      }
